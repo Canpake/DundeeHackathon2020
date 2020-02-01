a273afe1dc46eda5233cdb62aa5d67c5e5aaeea2
Jonathan Yim <canpake@users.noreply.github.com>
2020-02-01 16:03:22 +0000

    updated pulling country facts from csv files

--- tests/country_facts_csv.py
+++ tests/country_facts_csv.py
@@ -1,5 +1,23 @@
 import pandas as pd
 
 # Finding country facts through using a CSV file
-#
-df = pd.read_csv("")
+pd.options.display.max_rows = 10
+pd.options.display.max_columns = 10
+
+df = pd.read_csv("../data/country_profile_variables.csv")
+df = df.set_index(['country'])
+
+
+# population
+df['population'] = df['Population in thousands (2017)'] * 1000
+
+# gdp
+df = df.rename(columns={'GDP: Gross domestic product (million current US$)': 'gdp', 'Surface area (km2)': 'area'})
+
+df.loc[df['gdp'] == -99, 'gdp'] = "N/A"
+
+print(df[['population', 'gdp', 'area']])
+
+# retrieve countries only in trends by country
+country = pd.read_csv("../data/TrendsByCountry.csv")
+print(country)
