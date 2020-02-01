35927d48e09746263787c1cd18aa01d9da3b9b15
kanishkalikhanna <kanishkalikhanna@gmail.com>
2020-02-01 23:37:47 +0000

    arcade has coordinate calculations + shows text of country mouseover

---
+++ data/WikipediaDataGetter.py
@@ -0,0 +1,12 @@
+import wikipedia
+import requests
+import bs4
+
+
+def get_data(country):
+    print(wikipedia.summary(country, sentences = 5))
+
+
+get_data("United States")
+get_data("Myanmar")
+get_data("kiribas")
