1e5e98a773a4eb27a2f8657e8df3c05951afe5ee
Jonathan Yim <canpake@users.noreply.github.com>
2020-02-01 23:02:45 +0000

    updated main.py from test2.py. Flag now displays at a modified size and with some offset.

--- final/main.py
+++ final/main.py
@@ -4,28 +4,24 @@ from final import country_info
 from PIL import ImageTk, Image
 import requests
 from io import BytesIO
+import threading
+from time import sleep
 
+x = 0
+y = 0
 flag = None
+flag_url = "https://flagpedia.net/data/flags/normal/tg.png"
 
 def motion(event):
+    global x, y, flag_url
     x, y = event.x, event.y
-    lon = (x-184) * 302 / 939 - 124
+    lon = (x - 184) * 302 / 939 - 124
     lat = -(y) * 180 / 609 + 90
 
     country, facts, trends, flag_url = country_info.get_info(lat, lon)
 
-    response = requests.get(flag_url)
-    img_data = response.content
-
-    global flag
-    flag = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
-
-    image_canvas.delete("all")
-    image_canvas.create_image(593, 304, image=world_map)
-    image_canvas.create_image(x, y, anchor=tkinter.NW, image=flag)
-
     print(lat, lon)
-    print(country, facts, trends)
+    print(country, facts, trends, flag_url)
 
 
 def right_click(event):
@@ -43,16 +39,46 @@ def right_click(event):
     # pin_canvas.create_image(40, 60, image=pin)
 
 
+def update():
+    while True:
+        # set flag url
+        global flag_url
+        response = requests.get(flag_url)
+        img_data = response.content
+
+        # find flag image (and resize)
+        global flag
+        try:
+            current = ImageTk.PhotoImage(Image.open(BytesIO(img_data)).resize((int(550/10), int(367/10)), Image.ANTIALIAS))
+        except:
+            continue
+        flag = current
+
+        # redraw map + flag
+        image_canvas.delete("all")
+        image_canvas.create_image(0, 0, anchor=tkinter.NW, image=world_map)
+        image_canvas.create_image(x+30, y+15, anchor=tkinter.NW, image=flag)
+        sleep(0.01)
+
+
 root = Tk()
 root.geometry("1186x609")
 
+# set up canvas + load initial images
 image_canvas = Canvas(root, bg="blue", width=1186, height=609)
 image_canvas.place(relx=0, rely=0)
-
 world_map = ImageTk.PhotoImage(Image.open("../images/world_map.png"))
 image_canvas.create_image(593, 304, image=world_map)
 
+# bind actions
 root.bind('<Motion>', motion)
 root.bind("<Button-2>", right_click)
+
+# multithreading on flag loading
+threads = []
+a = threading.Thread(target=update)
+threads.append(a)
+a.start()
+
 root.mainloop()
 
